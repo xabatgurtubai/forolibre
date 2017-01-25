88631d88c986db9a810f9b97213bfe3887d91379
xabatgurtubai <xabatgurtubai@gmail.com>
2017-01-25 10:39:56 +0100

    Update showthread.php

--- showthread.php
+++ showthread.php
@@ -11,6 +11,7 @@ if(is_null($_SESSION['hilos'][$_GET['t']])){
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://m.forocoches.com/foro/showthread.php?t='.$_GET['t'].'&page='.$_GET['page']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
 curl_setopt($ch, CURLOPT_COOKIEFILE, 'login/cookie.txt');
 $response = utf8_encode(curl_exec($ch));
 curl_close($ch);
