a28e69c683337a0d1d8004ec7ae21c17b7b853af
xabatgurtubai <xabatgurtubai@gmail.com>
2017-01-25 10:32:20 +0100

    Update login.php
    Añadido user-agent.

--- login/login.php
+++ login/login.php
@@ -1,9 +1,11 @@
 <?php
-
+$usuario = "";
+$password = "";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://www.forocoches.com/foro/login.php?do=login');
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'vb_login_username=USERNAMEjdt&cookieuser=1&vb_login_password=PASSWORD&s=&securitytoken=guest&do=login&vb_login_md5password=&vb_login_md5password_utf=');
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'vb_login_username='.$usuario.'jdt&cookieuser=1&vb_login_password='.$password.'&s=&securitytoken=guest&do=login&vb_login_md5password=&vb_login_md5password_utf=');
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
 
